48500c6a8b665ef511cb82d63b102d51b97ff4fb
shawnsandy04 <shawnsandy04@gmail.com>
2017-07-18 22:30:07 -0400

    WIP

--- resources/views/theme/page/index.blade.php
+++ resources/views/theme/page/index.blade.php
@@ -7,13 +7,13 @@
 <section class="sub-header text-center">
 
     <div class="container">
-        <h1>
-            Just add content
-        </h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est error quae nostrum beatae, iusto accusantium repudiandae accusamus veritatis, voluptatum nesciunt dolorem aspernatur saepe a asperiores.
+        <h1> Just add content</h1>
+        <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est error quae nostrum beatae, iusto accusantium repudiandae accusamus veritatis, voluptatum nesciunt dolorem aspernatur saepe a asperiores.
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea doloribus similique officiis laudantium ratione praesentium. Voluptatibus, commodi saepe molestias ea iure optio dignissimos. Non, iusto.
         </p>
     </div>
+
 </section>
 
 @include($pageTheme.'shared.footer')
